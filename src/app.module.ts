16a41ee7c9f00cc0d9daf6c07345c7634861fab8
Mauricio Jourdan <jourdanmauricio@gmail.com>
2024-03-24 07:04:01 -0300

    Added Users, Customers, Brands and Categories

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,31 @@ import { AppService } from './app.service';
 import { ProductsController } from './controllers/products.controller';
 import { CategoriesController } from './controllers/categories.controller';
 import { ProductsService } from './services/products.service';
+import { CategoriesService } from './services/categories.service';
+import { BrandsService } from './services/brands.service';
+import { BrandsController } from './controllers/brands.controller';
+import { UsersController } from './controllers/users.controller';
+import { CustomersController } from './controllers/customers.controller';
+import { UsersService } from './services/users.service';
+import { CustomersService } from './services/customers.service';
 
 @Module({
   imports: [],
-  controllers: [AppController, ProductsController, CategoriesController],
-  providers: [AppService, ProductsService],
+  controllers: [
+    AppController,
+    ProductsController,
+    CategoriesController,
+    BrandsController,
+    UsersController,
+    CustomersController,
+  ],
+  providers: [
+    AppService,
+    ProductsService,
+    CategoriesService,
+    BrandsService,
+    UsersService,
+    CustomersService,
+  ],
 })
 export class AppModule {}
---
+++ src/dtos/customer.dto.ts
@@ -0,0 +1,19 @@
+import { IsString, IsNotEmpty } from 'class-validator';
+// IsPhoneNumber;
+import { PartialType } from '@nestjs/mapped-types';
+
+export class CreateCustomerDto {
+  @IsString()
+  @IsNotEmpty()
+  readonly name: string;
+
+  @IsString()
+  @IsNotEmpty()
+  readonly lastName: string;
+
+  // @IsPhoneNumber()
+  @IsNotEmpty()
+  readonly phone: string;
+}
+
+export class UpdateCustomerDto extends PartialType(CreateCustomerDto) {}
